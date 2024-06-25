dd11cdfca24e0df07c0dca75d83faeebac6cbaca
VWaghray <87341415+VWaghray@users.noreply.github.com>
2024-06-25 17:21:38 -0400

    Add files via upload

---
+++ PRO-C110-Project-Boilerplate.py
@@ -0,0 +1,46 @@
+import cv2
+
+import numpy as np
+
+import tensorflow as tf
+
+camera = cv2.VideoCapture(0)
+
+mymodel = tf.keras.models.load_model('keras_model.h5')
+
+while True:
+	status , frame = camera.read()
+
+	if status:
+
+		frame = cv2.flip(frame , 1)
+
+		resized_frame = cv2.resize(frame , (224,224))
+
+		resized_frame = np.expand_dims(resized_frame , axis = 0)
+
+		resized_frame = resized_frame / 255
+
+		predictions = mymodel.predict(resized_frame)
+
+		rock = int(predictions[0][0]*100)
+		paper = int(predictions[0][1]*100)
+		scizzor = int(predictions[0][2]*100)
+
+		if (rock>paper and rock>scizzor):
+			print("Rock")
+		elif (rock<paper and paper>scizzor):
+			print("Paper")
+		else:
+			print("Scizzor")
+
+		cv2.imshow('feed' , frame)
+
+		code = cv2.waitKey(1)
+		
+		if code == 32:
+			break
+
+camera.release()
+
+cv2.destroyAllWindows()
